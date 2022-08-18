bd0c7d3b1920a74f096feff20163d576a05a387f
mikemtzp <mikemp8226@gmail.com>
2022-08-18 15:51:20 -0500

    Add decode methods and fix linter errors

---
+++ morse_code.rb
@@ -0,0 +1,32 @@
+MORSE_CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
+  '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+  '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z', '.----' => '1', '..---' => '2', '...--' => '3', '....-' => '4',
+  '.....' => '5', '-....' => '6', '--...' => '7', '---..' => '8', '----.' => '9', '-----' => '0'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char]
+end
+
+def decode_word(morse_word)
+  word = ''
+  morse_word.split.each do |l|
+    word << decode_char(l)
+  end
+  word
+end
+
+def decode_sentence(morse_sentence)
+  sentence = ''
+  morse_sentence.split('   ').each do |w|
+    sentence << "#{decode_word(w)} "
+  end
+  sentence.strip
+end
+
+puts decode_char('--')
+puts decode_sentence('-- -.--   -. .- -- .')
+puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
